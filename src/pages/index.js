e365bd1110f7d7ffec74e18baedcfbb7bff3999b
injun lee <dldlswns890@gmail.com>
2022-05-11 22:10:37 +0900

    Add : 페이지 폴더 및 파일 생성

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import { Breadcrumb } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>로그인페이지</h1>
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">메인페이지</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">회원가입</Breadcrumb.Item>
+        <Breadcrumb.Item active>카카오로그인</Breadcrumb.Item>
+      </Breadcrumb>
+    </div>
+  );
+}
+
+export default App;
