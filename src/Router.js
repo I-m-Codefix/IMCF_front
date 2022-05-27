4b56eac6a2e5483b58eac77b49226c1bc77cda16
한범수 <han9119693@naver.com>
2022-05-27 21:47:23 +0900

    Update : 라우팅 path

--- src/Router.js
+++ src/Router.js
@@ -5,30 +5,30 @@ import Mainpage from "./pages/mainpage";
 import Usermypage from "./pages/usermypage";
 import Signuppage from "./pages/signuppage";
 import Modifyinformationpage from "./pages/modifyinformationpage";
-import Memberwithdrawalpage from "./pages/memberwithdrawalpage";
 import Cpapage from "./pages/cpapage";
 import Moviedescriptionpage from "./pages/moviedescriptionpage";
 import Movieapplicationpage from "./pages/movieapplicationpage";
 import Liveapplicationpage from "./pages/liveapplicationpage";
 import Playscreen_fullpage from "./pages/playscreen_fullpage";
 import Playscreen_windowpage from "./pages/playscreen_windowpage";
+import Memberwithdrawalpage from "./pages/memberwithdrawalpage";
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Loginpage />} />
-                <Route path="/signuppage" element={<Signuppage />} />
-                <Route path="/main" element={<Mainpage />} />
-                <Route path="/usermypage" element={<Usermypage />} />
-                <Route path="/modifyinformationpage" element={<Modifyinformationpage />} />
-                <Route path="/memberwithdrawalpage" element={<Memberwithdrawalpage />} />
-                <Route path="/cpapage" element={<Cpapage />} />
-                <Route path="/moviedescriptionpage" element={<Moviedescriptionpage />} />
-                <Route path="/movieapplicationpage" element={<Movieapplicationpage />} />
-                <Route path="/liveapplicationpage" element={<Liveapplicationpage />} />
-                <Route path="/playscreen_fullpage" element={<Playscreen_fullpage />} />
-                <Route path="/playscreen_windowpage" element={<Playscreen_windowpage />} />
+                <Route path="signup" element={<Signuppage />} />
+                <Route path="main" element={<Mainpage />} />
+                <Route path="/user" element={<Usermypage />} />
+                <Route path="delete" element={<Memberwithdrawalpage />} />
+                <Route path="modify" element={<Modifyinformationpage />} />
+                <Route path="cotentprovide" element={<Cpapage />} />
+                <Route path="infomovie" element={<Moviedescriptionpage />} />
+                <Route path="registmovie" element={<Movieapplicationpage />} />
+                <Route path="registlive" element={<Liveapplicationpage />} />
+                <Route path="playscreen_fullpage" element={<Playscreen_fullpage />} />
+                <Route path="playscreen_windowpage" element={<Playscreen_windowpage />} />
                 <Route path="*" element={<Notfound />} />
             </Routes>
         </BrowserRouter>
