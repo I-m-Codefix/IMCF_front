f2e71f5f4097a1f42623d9b3eee15ba2ec1b676a
한범수 <han9119693@naver.com>
2022-06-03 00:19:21 +0900

    Add : 에셋 이미지 & 로그인 페이지

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,15 +1,14 @@
 import { Breadcrumb, Card } from "react-bootstrap";
 import ButtonComponent from "../components/buttoncomponent";
 import InputGroupComponent from "../components/inputgroupcomponent";
-import MoviePosterComponent from "../components/moviepostercomponent";
+import bg from "../assets/image/bg1.jpg";
+import logo from "../assets/logo/logo_transparent.png";
 
-const divStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
+const bgStyle = {
     width: "100%",
     height: "100%",
+    backgroundImage: `url(${bg})`,
+    backgroundSize: "cover",
 };
 
 const cardStyle = {
@@ -17,39 +16,65 @@ const cardStyle = {
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    width: "40%",
+    width: "25%",
+    padding: "50px"
 };
 
 const rowStyle = {
     display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
     flexDirection: "row",
+    padding: "0 50px",
+}
+
+const overlayStyle = {
+    width: "100%",
+    height: "100%",
+    background: "rgba(0, 0, 0, 0.75)",
+}
+
+const wrapperStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+}
+
+const logoStyle = {
+    width: "500px",
+    height: "500px",
 }
 
 function Loginpage() {
     return (
-        <div style={divStyle}>
-            Loginpage
-            <Breadcrumb>
-                <Breadcrumb.Item href="/main">메인페이지</Breadcrumb.Item>
-                <Breadcrumb.Item href="/signuppage">회원가입</Breadcrumb.Item>
-                <Breadcrumb.Item active>카카오로그인</Breadcrumb.Item>
-            </Breadcrumb>
-            <Card style={cardStyle}>
-                <div style={rowStyle}>
-                    <div>
-                        <InputGroupComponent props = "ID"></InputGroupComponent>
-                        <InputGroupComponent props = "PWD"></InputGroupComponent>
-                    </div>
-                    <ButtonComponent props="로그인" /><br/>
+        <div style={bgStyle}>
+            <div style={overlayStyle}>
+                <div style={wrapperStyle}>
+                    <img src={logo} style={logoStyle} />
+                    Loginpage
+                    <Breadcrumb>
+                        <Breadcrumb.Item href="/main">메인페이지</Breadcrumb.Item>
+                        <Breadcrumb.Item href="/signuppage">회원가입</Breadcrumb.Item>
+                        <Breadcrumb.Item active>카카오로그인</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <Card style={cardStyle}>
+                        <div style={rowStyle}>
+                            <div>
+                                <InputGroupComponent props = "ID"></InputGroupComponent>
+                                <InputGroupComponent props = "PWD"></InputGroupComponent>
+                            </div>
+                            <ButtonComponent props="로그인" /><br/>
+                        </div>
+                        <div style={rowStyle}>
+                            <ButtonComponent props="회원가입" /><br/>
+                            <ButtonComponent props="카카오로그인" />
+                        </div>
+                    </Card>
                 </div>
-                <div style={rowStyle}>
-                    <ButtonComponent props="회원가입" /><br/>
-                    <ButtonComponent props="카카오로그인" />
-                </div>
-            </Card>
-            
-            {/* <MoviePosterComponent props="asdasd" title ="asssaa"></MoviePosterComponent> */}
-
+            </div>
         </div>
     );
 }
