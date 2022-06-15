e0fbacc1048643f64bd8e4f227679bd088d16698
한범수 <han9119693@naver.com>
2022-06-15 19:30:42 +0900

    Add : setValue 스토어 기능

--- src/pages/test.js
+++ src/pages/test.js
@@ -5,7 +5,12 @@ function Test() {
   const count = useStore((state) => state.counter);
 
   // 2번 방식
-  const { counter, plus } = useStore();
+  const { counter, plus, setValue } = useStore();
+
+  // 자바스크립트 기본
+  const onChange = (e) => {
+    setValue(e.target.value);
+  }
 
   return (
     <div className="Test">
@@ -17,6 +22,12 @@ function Test() {
       <div>
         <h1>2번 방식</h1>
         {counter}<br />
+        <button onClick={plus}>Plus Count</button>
+      </div>
+      <div>
+        <h1>값 할당</h1>
+        {counter}<br />
+        <input onChange={onChange} />
         <button onClick={plus}>Plus Count</button>
       </div>
     </div>
--- src/store/test.js
+++ src/store/test.js
@@ -1,11 +1,20 @@
 import create from "zustand";
 
+// 스토어
+// state, mutation, action
+// state(상태) = 변수 -> 내가 갖고 있을 값.
+// mutation = state를 변경하는 함수.
+// action = store를 조작하는 함수.
+
+// 리액트에서는 뮤테이션과 액션을 따로 구분하지 않음
+// 함수다 그래서 스테이트를 변경한다.
+
 const useStore = create((set) => ({
     counter: 0,
     plus: () => set((state) => {
         state.counter++;
     }),
-    setVlaue: (value) => set({
+    setValue: (value) => set({
         counter: value,
     }),
 }));
