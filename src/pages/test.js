3fc571b1d0945aeaa0c738e6e79ec94964bf33c0
injun lee <dldlswns890@gmail.com>
2022-07-28 20:02:08 +0900

    Update : 테스트 해보기

--- src/pages/test.js
+++ src/pages/test.js
@@ -36,10 +36,6 @@ const colorWhite = {
 }
 
 let inputValue = 0;
-<<<<<<< HEAD
-=======
-
->>>>>>> c99dda5bcb0398863589efe4228244b3d4b24468
 const onValue = (e) => {
   inputValue = e.target.value;
 }
@@ -49,27 +45,17 @@ function Test() {
   const counter = useStore(state => state.counter);
   const setValue = useStore(state => state.setValue);
   const plus = useStore(state => state.plus);
-<<<<<<< HEAD
-
-=======
-  const setValue = useStore(state => state.setValue);
->>>>>>> c99dda5bcb0398863589efe4228244b3d4b24468
   return (
     <TestLayout>
       <h1 style={colorWhite}>테스트1</h1>
       <div style={TestStyle}>
         <div style={colorWhite}>{counter}</div>
-<<<<<<< HEAD
         <div style={setValueStyle}>
           <div style={inputStyle}>
             <input id="input_setvalue" onChange={onValue} />
           </div>
           <Button variant="success" id="input_setvalue" onClick={() => setValue(inputValue)}>적용</Button>
         </div>
-=======
-        <input onChange={onValue} />
-        <Button variant="warning" onClick={() => setValue(inputValue)}>적용하기</Button>
->>>>>>> c99dda5bcb0398863589efe4228244b3d4b24468
         <Button variant="success" onClick={plus}>더하기</Button>
         <Button variant="secondary" onClick={() => { navigate("/test2") }}>Test2</Button>
       </div>
