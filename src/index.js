aa37a6bc935831b8445104c3459ef088eab70239
한범수 <han9119693@naver.com>
2022-08-29 20:16:37 +0900

    Update : React-Cookies install

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { CookiesProvider } from 'react-cookie';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import "./styles/Pages/index.scss"
@@ -10,11 +11,14 @@ import App from './App';
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </QueryClientProvider>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+  
 );
 
 export default App;
