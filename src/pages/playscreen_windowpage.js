63797d91a84e536e1689dbc7fd8564875288e077
한범수 <han9119693@naver.com>
2022-11-26 17:44:29 +0900

    Update : 영화 아이디 별 플레이 URL 바인딩

--- src/pages/playscreen_windowpage.js
+++ src/pages/playscreen_windowpage.js
@@ -2,25 +2,42 @@ import React from "react";
 import ReactPlayer from "react-player";
 import PlayLayout from "../layouts/playLayout";
 import { useParams } from "react-router";
+import { loadMovieData } from "../apis/api/movie";
+import { useCookies } from 'react-cookie';
 
 import "../styles/Pages/playscreen.scss";
+import { useQuery } from "react-query";
 
 export default function Playscreen_windowpage() {
-
+    const [cookies] = useCookies(['userInfo']);
+    const token = cookies.userInfo.token;
     const movieNum = useParams();
     console.log("영화 번호 : ", movieNum.movieId);
+    const { data, isLoading, error } = useQuery("moive", () => {
+        loadMovieData(token);
+        console.log(data);
+    });
 
-    return (
-        <PlayLayout>
-            <div className="video_wrapper">
-                <ReactPlayer
-                    className="react-player"
-                    url="https://d3uo0zm3pt2hib.cloudfront.net/streaming/video-ott.m3u8"
-                    width="100%"
-                    height="100%"
-                    controls={true}
-                />
+    if (isLoading) {
+        return (
+            <div>
+                <h1>Loading...</h1>
             </div>
-        </PlayLayout>
-    );
+        );
+    } else {
+        const playURL = data.result.playUri;
+        return (
+            <PlayLayout>
+                <div className="video_wrapper">
+                    <ReactPlayer
+                        className="react-player"
+                        url={playURL}
+                        width="100%"
+                        height="100%"
+                        controls={true}
+                    />
+                </div>
+            </PlayLayout>
+        );
+    }
 }
